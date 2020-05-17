9309e3561a57eb64da6443b8be5d7fad31faf0ba
Matteo Pierro <pierromatteo@gmail.com>
2020-05-17 20:27:57 +0200

    Add 2nd book of same type is 16 Euros

--- harrypotter.test.js
+++ harrypotter.test.js
@@ -2,6 +2,10 @@ it("charges 8 euros for 1 book",() => {
 	expect(checkout(1)).toBe(8);
 });
 
+it("charges 16 euros for 2 of the same books",() => {
+	expect(checkout(2)).toBe(16);
+});
+
 function checkout(numberOfBooks) {
-    return 8;
+    return numberOfBooks * 8;
 }
