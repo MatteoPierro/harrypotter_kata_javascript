fe595433ed07ce5f081b18007d54146c5d55afd8
Matteo Pierro <pierromatteo@gmail.com>
2020-05-17 20:38:18 +0200

    Add cart function and refactored test

--- harrypotter.test.js
+++ harrypotter.test.js
@@ -1,10 +1,23 @@
 it("charges 8 euros for 1 book",() => {
-	expect(checkout(1)).toBe(8);
+    const cart = Cart();
+
+    cart.add(FIRST_VOLUME);
+
+    expect(cart.amount()).toBe(8);
 });
 
 it("charges 16 euros for 2 of the same books",() => {
 	expect(checkout(2)).toBe(16);
 });
+
+const FIRST_VOLUME = "The Philosopher's Stone";
+
+function Cart() {
+    return {
+        add: (book) => {},
+        amount: () => 8
+    };
+}
 
 function checkout(numberOfBooks) {
     return numberOfBooks * 8;
