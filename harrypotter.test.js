25199b08aa344225504743626936f4d718728072
Nelis Boucké <nelis@archiwise.com>
2020-05-25 20:42:02 +0200

    Refactor to calculating normal & discount price books

--- harrypotter.test.js
+++ harrypotter.test.js
@@ -71,13 +71,15 @@ function Cart() {
             const uniqueBooks = new Set(books);
             if(uniqueBooks.size === 3){
                 discount = 0.9;
-            } else if(books.length !== 1 && books[0] !== books[1]) {
+            } else if(books.length === 2 && uniqueBooks.size === 2) {
                 discount = 0.95;
             } else if(books.length === 3 && books[1] !== books[2]) {
                 return 23.20;
             }
 
-            return 8 * books.length * discount;
+            let booksAtNormalPrice = books.length - uniqueBooks.size;
+            let booksAtDiscountPrice = uniqueBooks.size;
+            return 8 * booksAtNormalPrice + 8 * booksAtDiscountPrice  * discount;
         }
     };
 }
