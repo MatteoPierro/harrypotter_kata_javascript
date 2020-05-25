75359f4bd36025cb52506ebe8bc8807fec00f523
Nelis Boucké <nelis@archiwise.com>
2020-05-25 20:11:14 +0200

    Add test for 3 the same books

--- harrypotter.test.js
+++ harrypotter.test.js
@@ -34,6 +34,16 @@ it("charges 21.60 euros for 3 different books", () => {
     expect(cart.amount()).toBe(21.60);
 });
 
+it("charges 24.00 euros for 3 the same books",() => {
+    const cart = Cart();
+
+    cart.add(FIRST_VOLUME);
+    cart.add(FIRST_VOLUME);
+    cart.add(FIRST_VOLUME);
+
+	expect(cart.amount()).toBe(24.00);
+});
+
 const FIRST_VOLUME = "The Philosopher's Stone";
 const SECOND_VOLUME = "Harry Potter and the Chamber of Secrets";
 const THIRD_VOLUME = "Harry Potter and the Prisoner of Azkaban";
@@ -47,7 +57,7 @@ function Cart() {
         },
         amount: () => {
            let discount = 1;
-            if(books.length === 3) {
+            if(books.length === 3 && books[0] !== books[1]) {
                 discount = 0.9;
             }else if(books.length !== 1 && books[0] !== books[1]){
                 discount = 0.95;
